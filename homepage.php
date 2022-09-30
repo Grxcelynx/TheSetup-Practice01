a013bedf4f82bbceb11924d1acdaa07a1a5e2d2d
Grace Birnam <gbirnam@alphaworks.tech>
2022-09-29 17:22:46 -0700

    added base pages and homepage sass

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<?php  
+/**  
+* Template Name: Home  
+*  
+* @package TheSetup-Practice01 
+*  
+*/  
+?>  
+<?php get_header(); ?>
+<!--  code goes here  -->
+<h1>hello world test</h1>
+<?php get_footer();?>
