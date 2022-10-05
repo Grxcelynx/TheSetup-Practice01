4d0683f201f7f78a302197624791aa968fddb699
Grace Birnam <gbirnam@alphaworks.tech>
2022-10-04 17:09:55 -0700

    added nav

--- header.php
+++ header.php
@@ -29,24 +29,12 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$thesetup_practice01_description = get_bloginfo( 'description', 'display' );
-			if ( $thesetup_practice01_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $thesetup_practice01_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+		
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'thesetup-practice01' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"> <span class="burger-menu"> &#9776;</span></button>
 			<?php
 			wp_nav_menu(
 				array(
