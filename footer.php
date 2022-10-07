a5d06d2bf478e4c69bab00f8221ff4307e5a3011
Grace Birnam <gbirnam@alphaworks.tech>
2022-10-06 17:47:32 -0700

    added footer

--- footer.php
+++ footer.php
@@ -12,6 +12,29 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="footerBox">
+        	<?php
+            	wp_nav_menu(array(
+                	'menu' => 'Nav Menu',
+                	'theme_location' => 'footer-menu',
+                	'menu_class' => 'footer-menu',
+                	'menu_id' => 'footer-id'
+            	))
+        	?>
+
+        </div>
+        
+		<div class="footerBox">
+            <p>&copy; Grxcelyn Development</p>
+        </div>
+        <div class="footerBox">
+            <div class="socialMedia">
+                <a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>
+                <a href="https://twitter.com/i/flow/login" target="_blank"><i class="fa-brands fa-twitter-square fa-2x"></i></a>
+                <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-square fa-2x"></i></a>
+            </div>
+        </div>
+		
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'thesetup-practice01' ) ); ?>">
 				<?php
